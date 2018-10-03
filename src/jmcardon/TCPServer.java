afc145940e1ff11ef0e6185f38b8608f0a135b8c
root <root@localhost.localdomain>
2018-10-03 05:22:35 -0400

    kek

---
+++ src/jmcardon/TCPServer.java
@@ -0,0 +1,157 @@
+package jmcardon;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TCPServer {
+
+    private PrintStream log;
+    private volatile Set<InetAddress> firewall;
+    ServerSocket server;
+
+    public TCPServer(int port, PrintStream log, Set<InetAddress> firewall) throws IOException {
+
+        this.log = log;
+        this.firewall = firewall;
+        this.server = new ServerSocket(port);
+
+        InetAddress localHost = InetAddress.getLoopbackAddress();
+
+        insertLogEntry("\nServer Start", ipPortToString(server.getInetAddress(), port));
+
+        punch(localHost);
+
+    }
+
+    public void listen() {
+        while (!server.isClosed()) {
+            Socket client = createClient(server);
+
+            if (allowedIp(client)) {
+                Worker clientHandler = new Worker(this, client);
+                Thread workerThread = new Thread(clientHandler);
+                workerThread.start();
+            } else {
+                firewallViol(client);
+            }
+        }
+    }
+
+    public void closeServer() {
+        try {
+            server.close();
+            insertLogEntry("Server Shutdown", null);
+        } catch (IOException e) {
+            insertLogEntry(e.getMessage(), e.getStackTrace().toString());
+            System.out.println("Error " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Adds an IP Address to the fire wall for authorization.
+     *
+     * @param inetAddress an IP address that should be allowed to connect.
+     */
+    public void punch(InetAddress inetAddress) {
+        firewall.add(inetAddress);
+    }
+
+    /**
+     * Removes a previously authorized IP address from the fire wall.
+     *
+     * @param inetAddress an IP address that should no longer be allowed to connect.
+     */
+    public void plug(InetAddress inetAddress) {
+        firewall.remove(inetAddress);
+    }
+
+    /**
+     * Inserts an entry into the log file with the server's date and time.
+     *
+     * @param entry    main event that occurred (Server start, client connection,
+     *                 etc).
+     * @param subEntry additional information such as the client's IP address and
+     *                 port.
+     */
+    public void insertLogEntry(String entry, String subEntry) {
+        log.println(entry + " " + "(" + subEntry + ")" + " - " + getTime());
+    }
+
+    /**
+     * Retrieves the date and time of the server formatted into a string.
+     *
+     * @return A String containing the server's date and time.
+     */
+    public String getTime() {
+        ZonedDateTime currTime = ZonedDateTime.now();
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("E d MMM yyyy HH:mm:ss z");
+
+        String formattedTime = currTime.format(timeFormat);
+        return formattedTime;
+    }
+
+    private Socket createClient(ServerSocket server) {
+        Socket client = null;
+
+        try {
+            client = server.accept();
+        } catch (IOException e) {
+            insertLogEntry(e.getMessage(), e.getStackTrace().toString());
+            System.out.println(e.getMessage() + " shutting down");
+            System.exit(1);
+        }
+
+        return client;
+    }
+
+    private String ipPortToString(InetAddress ip, int port) {
+        String ipPort = ip.toString() + ":" + Integer.toString(port);
+
+        return ipPort;
+    }
+
+    private void firewallViol(Socket client) {
+        insertLogEntry("Firewall Violation", client.getInetAddress().toString());
+
+        try {
+            PrintStream clientOutput = new PrintStream(client.getOutputStream());
+            clientOutput.println("You are not authorized, closing");
+            client.close();
+        } catch (IOException e) {
+            insertLogEntry(e.getMessage(), e.getStackTrace().toString());
+            System.out.println(e.getMessage() + " shutting down");
+            System.exit(1);
+        }
+    }
+
+    private boolean allowedIp(Socket client) {
+        InetAddress clientIp = client.getInetAddress();
+        return firewall.contains(clientIp);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int listenPort = 10560;
+        String userHome = System.getProperty("user.home");
+        File logFile = new File(userHome, "/Documents/4413/log.txt");
+        Set<InetAddress> firewall = new HashSet<>();
+        PrintStream log = new PrintStream(new FileOutputStream(logFile, true));
+        System.out.println("Starting Server, connection port is " + listenPort);
+        TCPServer theServer = new TCPServer(listenPort, log, firewall);
+        theServer.listen();
+        theServer.closeServer();
+        System.out.println("Server shutting down");
+
+        log.close();
+    }
+}
---
+++ src/jmcardon/Worker.java
@@ -0,0 +1,199 @@
+package jmcardon;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.math.BigInteger;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import projA.Course;
+import projA.Util;
+
+public class Worker implements Runnable {
+
+    private TCPServer server;
+    private Socket client;
+
+    private String clientIp;
+    private Map<String, String> authMap;
+
+    /**
+     * Instantiates a worker to manage the socket, while leaving the server free to
+     * connect to other clients. Creates an authorization Map and implements dummy
+     * usernames and passwords for testing.
+     *
+     * @param server stores a reference to the TCP server for accessing methods
+     * @param client the socket corresponding to the connected client
+     */
+    public Worker(TCPServer server, Socket client) {
+        this.server = server;
+        this.client = client;
+        authMap = new HashMap<>();
+        authMap.put("root", "toor");
+    }
+
+    /**
+     * Each client should be connected on it's own thread. This method delegates to
+     * the handle method and catches all IO exceptions that may occur in the handle
+     * method.
+     */
+    public void run() {
+        try {
+            handle();
+        } catch (IOException e) {
+            server.insertLogEntry(e.getMessage(), e.getStackTrace().toString());
+            System.out.println(e.getMessage() + " shutting down");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Handles a connected client socket throughout the lifetime of the connection
+     * by first inserting a log entry with the client's IP, printing when the client
+     * connects and disconnects, and initiating IO between the client and server.
+     *
+     * @param client a client socket
+     * @throws IOException
+     */
+    public void handle() throws IOException {
+        clientIp = client.getInetAddress().toString();
+        server.insertLogEntry("Client Connnected", clientIp);
+
+        System.out.println("Client Connected");
+
+        clientIO();
+
+        System.out.println("Client Disconnected");
+    }
+
+    private String prime(int digits) {
+        final float BITS_PER_DIGIT = 3.00f;
+        int bitsToDec = (int) (BITS_PER_DIGIT * digits);
+
+        BigInteger bigPrime = BigInteger.probablePrime(bitsToDec, new Random());
+        String prime = bigPrime.toString();
+
+        return prime;
+    }
+
+    /**
+     * Responsible for sending and receiving input and output, to and from the
+     * client respectively. Sends the user input to the parseClient method and
+     * continues to ask for input until the parseClient method returns false, which
+     * occurs when the client enters 'exit' or 'bye'.
+     *
+     * @throws IOException if the server cannot get the client input or output
+     *                     stream.
+     */
+    private void clientIO() throws IOException {
+        PrintStream clientOutput = new PrintStream(client.getOutputStream());
+        Scanner clientScanner = new Scanner(client.getInputStream());
+
+        clientOutput.println("Please enter a command, type 'bye' to exit");
+        String clientInput = clientScanner.nextLine();
+
+        while (parseClient(clientInput, clientOutput)) {
+            clientOutput.println("Please enter another command");
+            clientInput = clientScanner.nextLine();
+        }
+
+        clientOutput.println("Disconnecting from server, have a nice day");
+        clientScanner.close();
+        bye();
+    }
+
+    /**
+     * Parses the client input using regex and either calls the matching function
+     * and sends out to the client, or tells the client their input was not valid.
+     *
+     * If the client enters bye, this method returns false.
+     *
+     * @param clientInput  the input string passed from the client to the server
+     * @param clientOutput a reference to the printstream object to return called
+     *                     method outputs
+     * @return true if the client enters any command, false if the client enters bye
+     */
+    private boolean parseClient(String clientInput, PrintStream clientOutput) {
+        boolean queryClient = true;
+
+        Pattern byePattern = Pattern.compile("(\\s*)(bye|exit)(\\s*)", Pattern.CASE_INSENSITIVE);
+        Matcher byeMatch = byePattern.matcher(clientInput);
+
+        Pattern timePattern = Pattern.compile("(\\s*)(get)(\\s*)(time)(\\s*)", Pattern.CASE_INSENSITIVE);
+        Matcher timeMatch = timePattern.matcher(clientInput);
+
+        Pattern primePattern = Pattern.compile("(\\s*)(prime)(\\s*)(\\d+)(\\s*)", Pattern.CASE_INSENSITIVE);
+        Matcher primeMatch = primePattern.matcher(clientInput);
+
+        Pattern authPattern = Pattern.compile("(\\s*)(auth)(\\s*)(\\S+)(\\s*)(\\S+)(\\s*)", Pattern.CASE_INSENSITIVE);
+        Matcher authMatch = authPattern.matcher(clientInput);
+
+        Pattern rosterPattern = Pattern.compile("(\\s*)(roster)(\\s*)(\\S+)(\\s*)", Pattern.CASE_INSENSITIVE);
+        Matcher rosterMatch = rosterPattern.matcher(clientInput);
+
+        if (byeMatch.matches()) {
+            queryClient = false;
+
+        } else if (timeMatch.matches()) {
+            String currTime = server.getTime();
+            clientOutput.println(currTime + "\n");
+
+        } else if (primeMatch.matches()) {
+            int clientDigits = Integer.parseInt(primeMatch.group(4));
+            String clientPrime = prime(clientDigits);
+
+            clientOutput.println(clientPrime + "\n");
+
+        } else if (authMatch.matches()) {
+            String userNameInput = authMatch.group(4);
+            String passInput = authMatch.group(6);
+
+            clientOutput.println(auth(userNameInput, passInput));
+
+        } else if (rosterMatch.matches()) {
+            String courseNum = rosterMatch.group(4);
+            String courseJson = roster(courseNum);
+            System.out.println(courseJson);
+            clientOutput.println(courseJson);
+        } else {
+            clientOutput.println("Don't understand <" + clientInput + ">\n");
+        }
+
+        return queryClient;
+
+    }
+
+    private String auth(String username, String password) {
+        if (authMap.containsKey(username)) {
+            String validPass = authMap.get(username);
+            if (validPass.matches(password)) {
+                return "You are in!";
+            }
+        }
+
+        return "Auth Failure!";
+    }
+
+    private String roster(String courseNum) {
+        Course course = Util.getCourse(courseNum);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String courseJson = gson.toJson(course);
+
+        return courseJson;
+    }
+
+    private void bye() throws IOException {
+        client.close();
+
+        server.insertLogEntry("Client Disconnected", clientIp);
+    }
+}
